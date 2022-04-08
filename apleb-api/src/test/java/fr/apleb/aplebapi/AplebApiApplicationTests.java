afd92032029aa877ad551b4795f0a2fcf684f9b8
baudu <bastien.audu@gmail.com>
2022-04-08 15:06:09 +0200

    initial commit

---
+++ apleb-api/src/test/java/fr/apleb/aplebapi/AplebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.apleb.aplebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
